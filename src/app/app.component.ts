68acc525010469de313b492eb74fd758b628151f
Steve Wang <npminitfailed@gmail.com>
2024-05-12 13:25:13 -0700

    added stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import * as jsonData from '../assets/question_sets.json';
 import {QuestionSetComponent} from "./question-set/question-set.component";
+import {MatButton} from "@angular/material/button";
+import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
 
 export interface Score {
   coverage: number;
@@ -37,12 +39,49 @@ export interface QuestionSet {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, QuestionSetComponent],
+  imports: [RouterOutlet, QuestionSetComponent, MatButton, MatMenuTrigger, MatMenu, MatMenuItem],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'NLP-Pipeline-Evaluation';
   data: QuestionSet[] = (jsonData as any).default;
-  dataToDisplay: QuestionSet[] = this.data.slice(0, 5);
+  dataToDisplay: QuestionSet[] = this.data.slice(0, 4);
+  allDataDisplayed = false;
+
+  loadMore() {
+    this.dataToDisplay = this.data;
+    this.allDataDisplayed = true;
+  }
+
+  sortByID() {
+    this.loadMore();
+    this.dataToDisplay.sort((a, b) => a.id - b.id);
+  }
+
+  sortByCoverage() {
+    this.loadMore();
+    this.dataToDisplay.sort((a, b) => b.coverage - a.coverage);
+  }
+
+  sortByPipelinePerformance() {
+    this.loadMore();
+    this.dataToDisplay.sort((a, b) => {
+      let aScore = a.pipelines[0].score.more_info;
+      let sum = 0;
+      for (let pipeline of a.pipelines.slice(1, a.pipelines.length)) {
+        sum += pipeline.score.more_info;
+      }
+      aScore -= sum / (a.pipelines.length - 1);
+
+      let bScore = b.pipelines[0].score.more_info;
+      sum = 0;
+      for (let pipeline of b.pipelines.slice(1, b.pipelines.length)) {
+        sum += pipeline.score.more_info;
+      }
+      bScore -= sum / (b.pipelines.length - 1);
+
+      return aScore - bScore;
+    });
+  }
 }
